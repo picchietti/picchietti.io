7a8c4b92441e24f8748b9abadab11dc79706c2f4
Jon Picchietti <jon@picchietti.email>
2016-03-16 23:30:04 -0500

    2016-03-16_23-30-04

---
+++ scripts/task.js
@@ -0,0 +1,129 @@
+var Task={
+
+add:function(task){
+	var xhr = new XMLHttpRequest();
+	xhr.open('POST', 'add.php', true);
+	xhr.setRequestHeader('Content-type','application/x-www-form-urlencoded');
+	xhr.send('task=' + encodeURIComponent(task));
+
+	xhr.onreadystatechange=function(){if(xhr.readyState==4 && xhr.status==200){
+		$('global').value = '';
+		Task.make(xhr.reponseText,task);
+	}}
+},
+
+check:function(task){
+	var xhr = new XMLHttpRequest();
+	xhr.open('POST', 'delete.php', true);
+	xhr.setRequestHeader('Content-type','application/x-www-form-urlencoded');
+	xhr.send('task='+encodeURIComponent(task.nextSibling.innerHTML));
+
+	xhr.onreadystatechange=function(){if(xhr.readyState==4 && xhr.status==200){
+		task.parentNode.parentNode.removeChild(task.parentNode);
+	}}
+},
+
+make:function(id,task){
+	var tasks = $('tasks');
+	var item = document.createElement('div');
+	var about = document.createElement('div');
+
+	var check = document.createElement('i');
+	check.className = 'check fa fa-check';
+
+	item.id = id;
+	item.className = 'item no-select';
+
+	about.innerHTML += task.replace('<','&lt;').replace('>','&gt;');
+	about.className = 'about';
+
+  about.mouse_move = function (){
+    var half_width = about.clientWidth * 0.25;
+    var diff = event.clientX - this.startX;
+    this.style.left = Math.max(0, diff) + 'px';
+    if(diff >= half_width)
+      check.classList.add('deleting');
+    else
+      check.classList.remove('deleting');
+  }
+
+  about.mouse_up = function(){
+    about.removeEventListener('mousemove', about.mouse_move, false);
+    document.removeEventListener('mouseup', about.mouse_up, false);
+    document.onselectstart = function(){return true;};
+    var half_width = about.clientWidth * 0.25;
+    var diff = event.clientX - about.startX;
+    if(diff >= half_width){
+      Task.check(check);
+      return;
+    }
+    else if(Math.abs(diff) <= 3){
+      var textarea = document.createElement('textarea');
+  		textarea.className = 'overlay';
+  		textarea.value = about.innerHTML;
+  		textarea.addEventListener('mousedown', function(event){event.stopPropagation();}, false);
+  		textarea.addEventListener('mousemove', function(event){event.stopPropagation();}, false);
+  		textarea.addEventListener('mouseup', function(event){event.stopPropagation();}, false);
+  		textarea.onkeydown=function(e){
+  			if(e.keyCode==13){
+  				var changed = this.value;
+  				Task.edit(this.parentNode.parentNode.id, changed);
+  				this.parentNode.innerHTML = changed.replace('<','&lt;').replace('>','&gt;');
+  			}
+  		}
+  		about.appendChild(textarea);
+  		textarea.focus();
+    }
+
+    about.style.left = "0px";
+  }
+
+	about.addEventListener('mousedown', function(){
+    this.startX = event.clientX
+    about.addEventListener('mousemove', about.mouse_move, false);
+    document.addEventListener('mouseup', about.mouse_up, false);
+    document.onselectstart = function(){return false;};
+	}, false);
+
+	item.appendChild(check);
+	item.appendChild(about);
+	tasks.insertBefore(item, tasks.firstChild);
+},
+
+edit:function(id,replaced){
+	var xhr = new XMLHttpRequest();
+	xhr.open('POST', 'edit.php', true);
+	xhr.setRequestHeader('Content-type','application/x-www-form-urlencoded');
+	xhr.send('replaced='+encodeURIComponent(replaced)+'&id='+id);
+},
+
+getAll:function(){
+	var xhr = new XMLHttpRequest();
+
+	xhr.open('GET', 'getAll.php', true);
+	xhr.send(null);
+
+	xhr.onreadystatechange=function(){if(xhr.readyState==4 && xhr.status==200){
+		var response = JSON.parse(xhr.responseText);
+
+		for(var i=0,y=response['id'].length;i<y;i++){
+			Task.make(response['id'][i],response['task'][i]);
+		}
+	}}
+}
+
+};
+
+function load(){
+	var input = $('global');
+	input.onkeydown=function(e){
+		if(e.keyCode == 13 && this.value != ''){
+			Task.add(this.value);
+		}
+	}
+
+	Task.getAll();
+	input.focus();
+}
+
+window.addEventListener('load',load,false);
--- tasks/index.php
+++ tasks/index.php
@@ -6,122 +6,25 @@
 @@include('../includes/head.php')
 <style>
 #global{font-size:16pt;width:90%;margin-bottom: 15px;}
-.overlay{position:absolute;width:100%;height:100%;left:0px;top:0px;font-family:inherit;font-size:inherit;resize:none;padding:0;}
-#list{width:100%;border-spacing:4px;}
-#list .check{cursor:pointer;margin-right:10px;color:#666;}
-#list .check:hover{color:#000;}
+.overlay{position:absolute;width:100%;height:100%;left:0px;top:0px;border: 0;outline:1px dashed #ccc;font-family:inherit;font-size:inherit;resize:none;padding:inherit;line-height:inherit;}
+#tasks{width:100%;cursor: default;font-size: 1em;line-height:1em;}
+#tasks .check, #tasks .about{padding: 10px 0;}
+#tasks .item{position: relative;overflow: hidden;}
+#tasks .check{display: block;background-color: #ddd;padding-left: 10px;}
+#tasks .deleting{color: #0f0;}
+#tasks .about{background-color: #fff;position:absolute;top:0;left:0;width:100%;padding-left:5px;}
 </style>
-<script>
-var Task={
-
-add:function(task){
-	var xhr = new XMLHttpRequest();
-	xhr.open("POST", "add.php", true);
-	xhr.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-	xhr.send("task="+encodeURIComponent(task));
-
-	xhr.onreadystatechange=function(){if(xhr.readyState==4 && xhr.status==200){
-		$('global').value = "";
-		Task.make(xhr.reponseText,task);
-	}}
-},
-
-check:function(task){
-	var xhr = new XMLHttpRequest();
-	xhr.open("POST", "delete.php", true);
-	xhr.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-	xhr.send("task="+encodeURIComponent(task.nextSibling.innerHTML));
-
-	xhr.onreadystatechange=function(){if(xhr.readyState==4 && xhr.status==200){
-		task.parentNode.parentNode.removeChild(task.parentNode);
-	}}
-},
-
-make:function(id,task){
-	var list = $("list");
-	var item = document.createElement("div");
-	var about = document.createElement("span");
-
-	var check = document.createElement("i");
-	check.className = "check fa fa-check";
-	check.addEventListener("click",function(){Task.check(this)},false);
-
-	item.id = id;
-	item.className = 'no-select'
-	item.style.position = "relative";
-
-	about.innerHTML += task.replace("<","&lt;").replace(">","&gt;");
-	about.addEventListener("click",function(){
-		var textarea = document.createElement("textarea");
-		textarea.className = "overlay";
-		textarea.value = this.innerHTML;
-		textarea.addEventListener("click", function(event){event.stopPropagation();}, false);
-		textarea.onkeydown=function(e){
-			if(e.keyCode==13){
-				var changed = this.value;
-				Task.edit(this.parentNode.parentNode.id,changed);
-				this.parentNode.innerHTML = changed.replace("<","&lt;").replace(">","&gt;");
-			}
-		}
-		this.appendChild(textarea);
-		textarea.focus();
-	}, false);
-
-	item.appendChild(check);
-	item.appendChild(about);
-	list.insertBefore(item,list.firstChild);
-},
-
-edit:function(id,replaced){
-	var xhr = new XMLHttpRequest();
-	xhr.open("POST", "edit.php", true);
-	xhr.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-	xhr.send("replaced="+encodeURIComponent(replaced)+"&id="+id);
-},
-
-getAll:function(){
-	var xhr = new XMLHttpRequest();
-
-	xhr.open("GET", "getAll.php", true);
-	xhr.send(null);
-
-	xhr.onreadystatechange=function(){if(xhr.readyState==4 && xhr.status==200){
-		var response=JSON.parse(xhr.responseText);
-
-		for(var i=0,y=response["id"].length;i<y;i++){
-			Task.make(response["id"][i],response["task"][i]);
-		}
-	}}
-}
-
-};
-
-
-function load(){
-	var input = $('global');
-	input.onkeydown=function(e){
-		if(e.keyCode==13 && this.value!=""){
-			Task.add(this.value);
-		}
-	}
-
-	Task.getAll();
-	input.focus();
-}
-
-window.addEventListener('load',load,false);
-
-</script>
+<script src="//www.jonpicchietti.com/lib/underscore.min.js"></script>
+<script src="//www.jonpicchietti.com/scripts/task.js"></script>
 </head>
 <body>
 @@include('../includes/header.php')
 <div id="content">
-	<div style="text-align:center;">
+	<div class="alignc">
 		<input type="text" id="global" placeholder="To do..." />
 	</div>
 
-	<div id="list"></div>
-	<div id="pushfoot"></div>
+	<div id="tasks"></div>
 </div>
 
 @@include('../includes/footer.php')
