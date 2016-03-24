bca96f35d08ee720ec114ca9efd8a62a5decb107
Jon Picchietti <jon@picchietti.email>
2016-03-23 22:10:28 -0500

    2016-03-23_22-10-28

--- upload/cred.php
+++ upload/cred.php
@@ -1,6 +1,6 @@
 <?php
 
 if(!isset($_COOKIE["PHPSESSID"]) || !session_start() || $_SESSION["user"]!="jon@picchietti.email")
-	header("location:https://www.picchietti.io/login/?r=https://www.picchietti.io/upload/");
+	header("location:https://picchietti.io/login/?r=https://picchietti.io/upload/");
 
 ?>
