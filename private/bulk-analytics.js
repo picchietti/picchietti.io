0ce814aa5abc9fff0a5196f1db3a1c923af632bb
Jon Picchietti <jon@picchietti.email>
2017-04-06 15:28:58 -0500

    2017-04-06_15-28-58

--- private/bulk-analytics.js
+++ private/bulk-analytics.js
@@ -15,39 +15,60 @@ var jwtClient = new google.auth.JWT(
 
 var Analytics = {
   yesterday: moment().subtract(1, 'days'),
+  todo: [], // each item is an array with id, source, and since strings
 
-  save: function(id, source, since){
-    since = moment(since);
+  doAll: function(){
+    if(!Analytics.todo.length)
+      return;
 
-    while (since.isSameOrBefore(Analytics.yesterday)) {
-      var since_string = since.format('YYYY-MM-DD');
-      var params = {
-        "auth": jwtClient,
-        "ids": 'ga:' + id,
-        "start-date": since_string,
-        "end-date": since_string,
-        "metrics": 'ga:users,ga:pageviews'
-      };
+    var todo = Analytics.todo.shift();
+    var source = todo[1];
+    var since = moment(todo[2]);
 
-    	Analytics.get(params, source);
-    	since.add(1, 'days');
-    	sleep.sleep(2);
+    console.log('getting analytics for...', source);
+
+    if(since.isSameOrBefore(Analytics.yesterday)) {
+    	Analytics.get(todo[0], source, since);
     }
   },
 
-  get: function(params, source){
+  get: function(id, source, since){
+    console.log('getting analytics for', source, since.format('YYYY-MM-DD'));
+
+    var since_string = since.format('YYYY-MM-DD');
+    var params = {
+      "auth": jwtClient,
+      "ids": 'ga:' + id,
+      "start-date": since_string,
+      "end-date": since_string,
+      "metrics": 'ga:users,ga:pageviews'
+    };
+
     analytics.data.ga.get(params, function(err, response){
+      console.log('got analytics for', source, since_string);
       totals = response.totalsForAllResults;
-      Analytics.store(totals, source, params['start-date']);
+      Analytics.store(totals, source, since_string);
+
+      since.add(1, 'days')
+      if(since.isSameOrBefore(Analytics.yesterday)){
+        sleep.sleep(5);
+        Analytics.get(id, source, since);
+      }
+      else {
+        console.log('...done getting analytics for', source);
+        Analytics.doAll();
+      }
     });
   },
 
   store: function(totals, source, when){
     db.getConnection(function(err, conn){
+      if(err) throw err;
       var users = parseInt(totals['ga:users']);
       var pageviews = parseInt(totals['ga:pageviews']);
 
       conn.query("INSERT INTO impact_analytics (pageviews, users, source, ymd) VALUES (?, ?, ?, ?)", [pageviews, users, source, when], function(err, result){
+        if(err) throw err;
         conn.release();
       });
     });
@@ -60,10 +81,15 @@ jwtClient.authorize(function (err, tokens) {
     return;
   }
 
-  Analytics.save('41469908', 'siualumni.com', '2016-11-22'); // established '2013-10-30'
-  Analytics.save('82388800', 'dnadiscovery.net', '2016-11-22'); // established '2014-02-18'
-  Analytics.save('118734877', 'picchietti.io', '2016-11-22'); // established '2016-03-26'
-  
-  // Analytics.save('74075825', 'msknighteducation.com', '2016-11-22'); // established '2013-07-02'
-  // Analytics.save('71729805', 'jonpicchietti.com', '2016-11-22'); // established '2013-04-26'
+  // api allows 100 requests in 100 seconds
+  // sleep from one source seems to interfere with another's db.getConnection handshake
+  Analytics.todo.push(['41469908', 'siualumni.com', '2016-11-22']); // established '2013-10-30'
+  Analytics.todo.push(['82388800', 'dnadiscovery.net', '2016-11-22']); // established '2014-02-18'
+  Analytics.todo.push(['118734877', 'picchietti.io', '2016-11-22']); // established '2016-03-26'
+
+  Analytics.doAll();
 });
+
+// // Legacy
+// Analytics.save('74075825', 'msknighteducation.com', '2016-11-22'); // established '2013-07-02'
+// Analytics.save('71729805', 'jonpicchietti.com', '2016-11-22'); // established '2013-04-26'
