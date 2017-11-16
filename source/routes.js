6cd35deed09403927e852d4c7c419e2c5fc4071d
Jon Picchietti <jon@picchietti.email>
2017-11-15 20:42:14 -0600

    Remove bookmarks
    I am removing this in favor of firefox sync which encrypts all bookmarks
    stored remotely.
    
    I might add a dashboard or a top sites feature in its place.

--- source/routes.js
+++ source/routes.js
@@ -5,15 +5,6 @@ module.exports = function(router, passport, allowUpload){
   router.post('/upload/file', restrictAccess, allowUpload);
   router.use('/upload/url', restrictAccess, require(root_dir + '/api/upload/url.js'));
 
-  router.use('/bookmarks/delete', restrictAccess, require(root_dir + '/api/bookmarks/delete.js'));
-  router.use('/bookmarks/update', restrictAccess, require(root_dir + '/api/bookmarks/update.js'));
-  router.use('/bookmarks/move', restrictAccess, require(root_dir + '/api/bookmarks/move.js'));
-  router.use('/bookmarks/add', restrictAccess, require(root_dir + '/api/bookmarks/add.js'));
-  router.use('/bookmarks/folders', restrictAccess, require(root_dir + '/api/bookmarks/folders.js'));
-  router.use('/bookmarks/get/:folder/:amount', restrictAccess, require(root_dir + '/api/bookmarks/get.js'));
-  router.use('/bookmarks/list/:folder/:amount', restrictAccess, require(root_dir + '/api/bookmarks/list.js'));
-  router.use('/bookmarks/more/:folder/:amount/:total', restrictAccess, require(root_dir + '/api/bookmarks/more.js'));
-
   router.use('/data/month/users', require(root_dir + '/api/data/month/users.js'));
   router.use('/data/month/pageviews', require(root_dir + '/api/data/month/pageviews.js'));
 
@@ -27,9 +18,6 @@ module.exports = function(router, passport, allowUpload){
     res.redirect('/');
   });
 
-  router.get('/pages/bookmarks', restrictAccess, function(req, res) {
-    res.sendFile(root_dir + '/public/pages/bookmarks/');
-  });
   router.get('/pages/upload', restrictAccess, function(req, res) {
     res.sendFile(root_dir + '/public/pages/upload/');
   });
