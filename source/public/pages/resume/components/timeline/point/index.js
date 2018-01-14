29f6dd84e14cd9d5061d1dc022d4b895056adfea
Jon Picchietti <jon@picchietti.email>
2018-01-14 12:57:20 -0600

    Allow 'current' job in employment timeline
    A point in a timeline no longer needs to have an end date. This means
    that it is still ongoing and defaults to being 'Current'.

--- source/public/pages/resume/components/timeline/point/index.js
+++ source/public/pages/resume/components/timeline/point/index.js
@@ -4,22 +4,33 @@ import PropTypes from 'prop-types';
 export default class Point extends React.Component {
   static propTypes = {
     from: PropTypes.string.isRequired, // string with YYYY at end
-    to: PropTypes.string.isRequired, // string with YYYY at end
+    to: PropTypes.string, // string with YYYY at end
     description: PropTypes.any.isRequired, // string or jsx
     bullets: PropTypes.array.isRequired, // array of (strings or jsx)
     isFeatured: PropTypes.bool
   }
 
+  static defaultProps = {
+    to: 'Current'
+  }
+
   constructor(props) {
     super(props);
 
-    this.shortDateRange = props.from.slice(-2) + '-' + props.to.slice(-2);
+    this.shortDateRange = this.getShortDateRange();
     this.longDateRange = props.from + ' - ' + props.to;
     this.iconClass = 'fa fa-stack-1x foreground ' + ((props.isFeatured) ? 'featured fa-star' : 'fa-circle');
 
     this.bullets = props.bullets.map((bullet, i) => (
       <li key={i}>{bullet}</li>
     ));
+  }
+
+  getShortDateRange() {
+    if(this.props.to === Point.defaultProps.to)
+      return this.props.from + '+';
+    else
+      return this.props.from.slice(-2) + '-' + this.props.to.slice(-2);
   }
 
   render() {
