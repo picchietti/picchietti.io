8d6f2d704e301dc08b7fb7ef0acf3537c7524ba1
Jon Picchietti <jon@picchietti.email>
2018-02-05 23:32:21 -0600

    Convert login to mongo

--- source/private/passport.js
+++ source/private/passport.js
@@ -1,7 +1,6 @@
 const crypto = require('crypto');
 const LocalStrategy = require('passport-local').Strategy;
-
-var db = require('/usr/src/app/private/database.js');
+const mongo = require('/usr/src/app/private/mongodb.js');
 
 function sha512(data) {
   return crypto.createHash('sha512').update(data).digest('hex');
@@ -20,36 +19,27 @@ module.exports = function(passport){
     function(username, password, done) {
       // find a way to throttle failed attempts or reimplement own
 
-      db.getConnection(function(err, conn){
-        if(err) {
-          conn.release();
-          return done(err);
-        }
-
-        conn.query('SELECT password,salt FROM accounts WHERE email=?', [username], function(err, rows, fields){
+      mongo.getConnection().then( (db) => {
+        db.collection('users').findOne({ email: username }, function(err, user){
           if(err) {
-            conn.release();
             return done(err);
           }
 
-          if(!rows.length){ // cant find user
-            conn.release();
+          if(!user){ // cant find user
             return done(null, false, { message: 'Incorrect username.' });
           }
 
-          var stored_password = rows[0].password;
-          var salt = rows[0].salt;
+          var stored_password = user.password;
+          var salt = user.salt;
           var hash = sha512(password + salt);
 
           if(hash === stored_password){ // correct password
-            conn.release();
             done(null, {username: username});
           }
           else{ // wrong password
-            conn.release();
             done(null, false, { message: 'Incorrect password.' });
           }
-        });
+        } );
       });
     }
   ));
