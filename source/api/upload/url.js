43b1c6dc4ae2b78c209b7ee3598eddc29e6dea5c
Jon Picchietti <jon@picchietti.email>
2018-02-28 19:24:43 -0600

    Change url upload to request

--- source/api/upload/url.js
+++ source/api/upload/url.js
@@ -1,9 +1,7 @@
 var express = require('express');
 var router = express.Router();
-var http = require('http');
-var https = require('https');
-var url = require('url');
 var fs = require('fs');
+var request = require('request');
 
 const root_dir = '/usr/src/app';
 
@@ -11,30 +9,19 @@ router.post('/', function(req, res){
   var file_url = req.body.url;
   var filename = Date.now() + ' - ' + file_url.substring(file_url.lastIndexOf('/') + 1);
   var path = root_dir + '/secret/uploads/';
+
   var file = fs.createWriteStream(path + filename);
+  file.on('open', function(fd){
+    file.on('finish', function(){
+      file.close();
+      res.status(204).end();
+    })
 
-  file.on('open', function(){
-    var request_done = function(response) {
-      file.on('finish', function(){
-        file.close();
-        res.status(204).end();
-      });
-
-      response.pipe(file);
-    };
-
-    var request, protocol = url.parse(file_url).protocol;
-    if(protocol == 'http:')
-      request = http.get(file_url, request_done);
-    else if(protocol == 'https:')
-      request = https.get(file_url, request_done);
-    else
-      return res.status(400).end();
-
-    request.on('error', function(err){
+    request(file_url).on('error', function(err) {
+      file.close();
       fs.unlink(path + filename);
       res.status(500).end();
-    });
+    }).pipe(file);
   });
 });
 
