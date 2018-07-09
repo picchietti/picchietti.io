71592cad9aa5fdc5c43459420abf939d44859f41
Jon Picchietti <jon@picchietti.email>
2018-07-08 19:55:53 -0500

    Prioritize page content over analytics
    Analytics used to be fetched and collected in the middle of the page 
    load.
    
    This speeds up the site:
    -By importing the analytics fetching code into the project instead of 
    requesting it at page load time.
    -Delaying both analytics requests until after all  page requests.

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 
 import App from './public/components/app';
 import setupFontAwesome from './public/scripts/font-awesome';
+import setupAnalytics from './public/scripts/analytics';
 
 import './public/index.scss';
 
 setupFontAwesome();
+
+setTimeout(setupAnalytics, 500);
 
 ReactDOM.render(
   <App />,
